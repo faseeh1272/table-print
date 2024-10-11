b2ca034dc44b0ec5751ba2b1550fe43242286b9d
faseeh1272 <syedfaseehnaqvi2@gmail.com>
2024-10-11 14:33:51 -0700

    changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ for (var i = 1; i<=userValue; userValue--){
 
 for (var j = 1; j<=10; j++){
 
-    document.write("<br>"), userValue  + " x " + j + " = " + userValue*j;
+    document.write("<br>", userValue  + " x " + j + " = " + userValue*j);
 }
 document.write("<br>")
 }
