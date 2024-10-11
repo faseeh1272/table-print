b23cfc7c50c8ab96a5fa69c97411b3c73a397115
faseeh1272 <syedfaseehnaqvi2@gmail.com>
2024-10-11 14:14:54 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var userValue = prompt("Enter a number and generate its value:");
+
+for (var i = 1; i<=userValue; i--){
+
+for (var j = 1; j<=10; j++){
+
+    document.write("<br>"), userValue  + " x " + j + " = " + userValue*j;
+}
+document.write("<br>")
+}
