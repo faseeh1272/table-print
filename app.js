3649ba77961c04f55c798a840ffdcffcb38567e2
faseeh1272 <syedfaseehnaqvi2@gmail.com>
2024-10-11 14:31:04 -0700

    some changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var userValue = prompt("Enter a number and generate its value:");
 
-for (var i = 1; i<=userValue; i--){
+for (var i = 1; i<=userValue; userValue--){
 
 for (var j = 1; j<=10; j++){
 
